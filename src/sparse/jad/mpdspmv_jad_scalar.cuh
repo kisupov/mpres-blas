9345d604fa92d09834a5ef422495f8d33f2cd691
Ivan Babeshko <harigami@mail.ru>
2021-03-29 15:48:27 +0300

    added jad conversion and campary-double, mpres-double and taco tests

--- src/sparse/jad/mpdspmv_jad_scalar.cuh
+++ src/sparse/jad/mpdspmv_jad_scalar.cuh
@@ -40,16 +40,16 @@ namespace cuda {
      * @note No global memory buffer is required
      *
      * @param m - number of rows in matrix
-     * @param maxnzr - maximum number of nonzeros per row
+     * @param nzr - maximum number of nonzeros per row
      * @param offset - offset for diagonals
      * @param as - multiple-precision coefficients array (entries of the matrix A in the JAD (JDS) format), size nnz (number of nonzeros in matrix)
      * @param ja - column indices array to access the corresponding elements of the vector x, size = nnz
-     * @param jcp - col start pointers array of size maxnzr + 1, last element of jcp equals to nnz
+     * @param jcp - col start pointers array of size nzr + 1, last element of jcp equals to nnz
      * @param perm_rows - permutated row indices, size = m
      * @param x - input vector, size at least max(ja) + 1, where max(ja) is the maximum element from the ja array
      * @param y - output vector, size at least m
      */
-    __global__ void mpdspmv_jad_scalar(const int m, const_int maxnzr, const double *as, const int *ja, const int *jcp, const int *perm_rows, mp_float_ptr x, mp_float_ptr y) {
+    __global__ void mpdspmv_jad_scalar(const int m, const int nzr, const double *as, const int *ja, const int *jcp, const int *perm_rows, mp_float_ptr x, mp_float_ptr y) {
         auto row = threadIdx.x + blockIdx.x * blockDim.x;
         while (row < m) {
             int j = 0;
@@ -57,10 +57,11 @@ namespace cuda {
             mp_float_t prod;
             mp_float_t dot = cuda::MP_ZERO;
 
-            while (j < maxnzr && index < jcp[j + 1]) {
+            while (j < nzr && index < jcp[j + 1]) {
                 cuda::mp_mul_d(&prod, &x[ja[index]], as[index]);
                 cuda::mp_add(&dot, &dot, &prod);
-                index = row + jcp[j++];
+                index += jcp[j+1] - jcp[j];
+                j++;
             }
 
             cuda::mp_set(&y[perm_rows[row]], &dot);
