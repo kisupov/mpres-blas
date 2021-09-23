5645da3bab62408ce30a1cfad83b302e33eb5ab1
kisupov <isupov.k@gmail.com>
2021-09-23 10:45:24 +0300

    Add multiple precision subtraction (mp_sub)

---
+++ src/arith/sub.cuh
@@ -0,0 +1,59 @@
+/*
+ *  Multiple-precision floating-point subtraction using Residue number system
+ *
+ *  Copyright 2021 by Konstantin Isupov.
+ *
+ *  This file is part of the MPRES-BLAS library.
+ *
+ *  MPRES-BLAS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MPRES-BLAS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MPRES-BLAS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MPRES_SUB_CUH
+#define MPRES_SUB_CUH
+
+#include "add.cuh"
+
+/*!
+ * Subtraction of two multiple-precision numbers
+ * result = x - y
+ */
+GCC_FORCEINLINE void mp_sub(mp_float_ptr result, mp_float_ptr x, mp_float_ptr y) {
+
+   mp_float_t ly = * y;
+   ly.sign = ly.sign^1;
+   mp_add(result, x, &ly);
+}
+
+/*
+* GPU functions
+*/
+namespace cuda {
+    /*!
+     * Subtraction of two multiple-precision numbers
+     * result = x - y
+     */
+    DEVICE_CUDA_FORCEINLINE void mp_sub(mp_float_ptr result, mp_float_ptr x, mp_float_ptr y) {
+        er_float_ptr evalx[2] = { &x->eval[0], &x->eval[1] }; //Array of pointers to interval evaluations
+        er_float_ptr evaly[2] = { &y->eval[0], &y->eval[1] };
+        er_float_ptr evalr[2] = { &result->eval[0], &result->eval[1] };
+        mp_add_common(&result->sign, &result->exp, evalr, result->digits,
+                      x->sign, x->exp, evalx, x->digits,
+                      y->sign^1, y->exp, evaly, y->digits); //Sign inversion is performed
+        if (result->eval[1].frac != 0 && result->eval[1].exp >= cuda::MP_H) {
+            cuda::mp_round(result, cuda::mp_get_rnd_bits(result));
+        }
+    }
+}
+
+#endif //MPRES_SUB_CUH
---
+++ tests/arith/test_mp_sub_verify.cu
@@ -0,0 +1,72 @@
+/*
+ *  Test for validating the mp_sub routines
+ *
+ *  Copyright 2021 by Konstantin Isupov.
+ *
+ *  This file is part of the MPRES-BLAS library.
+ *
+ *  MPRES-BLAS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MPRES-BLAS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MPRES-BLAS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include "../../src/arith/sub.cuh"
+#include "../../src/arith/assign.cuh"
+
+static __global__ void testCudaSub(mp_float_ptr dz, mp_float_ptr dx, mp_float_ptr dy){
+    cuda::mp_sub(dz, dx, dy);
+}
+
+int main() {
+    rns_const_init();
+    mp_const_init();
+    rns_const_print(true);
+    rns_eval_const_print();
+
+    mp_float_t x, y, z;
+    mp_set_d(&x, 1.1e-10);
+    mp_set_d(&y, 1.1e10);
+
+    printf("\nARG X = %.16f", mp_get_d(&x));
+    //mp_print(&x);
+    //printf("\n");
+
+    printf("\nARG Y = %.16f", mp_get_d(&y));
+    //mp_print(&y);
+    //printf("\n");
+
+    //CPU SUB
+    mp_sub(&z, &x, &y);
+    printf("\nCPU RESULT  = %.16f", mp_get_d(&z));
+    //mp_print(&z);
+    //printf("\n");
+
+    mp_float_ptr dx;
+    mp_float_ptr dy;
+    mp_float_ptr dz;
+    cudaMalloc(&dx, sizeof(mp_float_t));
+    cudaMalloc(&dy, sizeof(mp_float_t));
+    cudaMalloc(&dz, sizeof(mp_float_t));
+    cudaMemcpy(dx, &x, sizeof(mp_float_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(dy, &y, sizeof(mp_float_t), cudaMemcpyHostToDevice);
+
+    //CUDA SUB
+    testCudaSub<<<1,1>>>(dz, dx, dy);
+    mp_set_d(&z, 0.0);
+    cudaMemcpy(&z, dz, sizeof(mp_float_t), cudaMemcpyDeviceToHost);
+    printf("\nCUDA RESULT = %.16f", mp_get_d(&z));
+    //mp_print(&z);
+    //printf("\n");
+
+    return 0;
+}
